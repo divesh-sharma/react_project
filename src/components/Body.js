a893162f38c3dce9dc0addce29dca90947c3963b
divesh-sharma <diveshsharma157@gmail.com>
2023-12-29 17:45:00 +0530

    useState,useEffect,Shimmer Effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,41 @@
 import { resList } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const TOP_RATED = 4;
 
 
 
 
 const Body = ()=>{
-    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants,setListOfRestaurants] = useState([]);
     const setTopRatedRestuarent = ()=>{
         const list= listOfRestaurants.filter(resObj=>resObj.avgRating>= TOP_RATED);
         console.log(list);
         setListOfRestaurants(list);
         
     }
-    return (
+    useEffect(()=>{
+        console.log("useEffect called");
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=>{
+        // const data =await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+
+        // const json = await data.json();
+        // options chaining
+        // console.log(json?.data?.cards);
+        setTimeout(()=>{
+            setListOfRestaurants(resList);
+        },1000)
+    }
+// Condition Rendering 
+    // if(listOfRestaurants.length==0){
+    //     return <Shimmer />
+    // }
+    return listOfRestaurants.length==0 ? (<Shimmer />):
+    (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn"
